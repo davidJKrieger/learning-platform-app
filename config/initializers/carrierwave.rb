dbad8b877eabd0dad02a49f8bc5f86be917f117d
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-09-17 12:43:00 -0600

    added video upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,22 @@
-# config/initializers/carrierwave.rb
+
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  config.storage    = :aws
+  config.aws_bucket = ENV["image-upload-awake"]
+  config.aws_acl    = "public_read"
+
+  config.aws_credentials = {
+    access_key_id:     ENV["AKIAUVV45DEEHHHOUX7Y"],        # required
+    secret_access_key: ENV["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"],        # required
+    region:            ENV["us-west-1"]
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
+
+
+
+
+
+#     aws_access_key_id:     ENV["AKIAUVV45DEEHHHOUX7Y"],        # required
+#     aws_secret_access_key: ENV["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"],
+
+# region:                ENV["us-east-1"] 
